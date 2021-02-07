6dce67c55c91fc991d6e1b98833dbc62884fd0b5
albertot_dev <atejero.dam@gmail.com>
2021-02-07 09:47:24 +0100

    added pre config

---
+++ src/environments/environment.pre.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  client_id: 'c36a09cd0a6accfa2551',
+  github_redirect_url: 'https://gameofchars-pre.herokuapp.com/auth/github',
+  url_backend: 'https://gameofchars-pre.herokuapp.com'
+};
