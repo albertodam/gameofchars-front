0deb9f863592e86df4572d61b6d80cbd5b9dff9b
albertot_dev <atejero.dam@gmail.com>
2021-02-28 17:35:43 +0100

    added testing library and workflow github actions

--- src/app/base/header/header.component.spec.ts
+++ src/app/base/header/header.component.spec.ts
@@ -1,25 +1,71 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterModule } from '@angular/router';
+import { fireEvent, render, screen } from '@testing-library/angular';
+import { AppRoutingModule } from './../../app-routing.module';
+import { GithubComponent } from './../github/github.component';
 import { HeaderComponent } from './header.component';
-
 describe('HeaderComponent', () => {
-  let component: HeaderComponent;
-  let fixture: ComponentFixture<HeaderComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async () => {
+        await render(HeaderComponent, {
+            declarations: [
+                GithubComponent
+            ],
+            imports: [
+                AppRoutingModule,
+                RouterModule
+            ]
+        });
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HeaderComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    describe('when user not logged', () => {
+        it('should show button Login Github', async () => {
+            expect(screen.getByText('Login Github'));
+        });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+        it('should open github window when click in button login', () => {
+            window.open = jest.fn();
+            fireEvent.click(screen.getByText('Login Github'));
+            expect(window.open).toBeCalled();
+
+        });
+
+        it(('shouldnt show avatar image'), () => {
+            const containerImg = document.querySelector('#user-avatar');
+            expect(containerImg).toBeNull();
+        });
+
+        it(('shouldnt show user name'), () => {
+            expect(screen.queryByText('User Fake')).toBeNull();
+        });
+
+    });
+
+    describe('when user logged', () => {
+
+        beforeAll(async () => {
+            const userFake = {
+                username: 'User Fake',
+                avatar: 'https://avatar-fake.png',
+                id: 'id-fake'
+            };
+            localStorage.setItem('userInfo', JSON.stringify(userFake));
+
+        });
+
+        it('shouldnt show button Login Github', () => {
+            expect(screen.queryByText('Login Github')).toBeNull();
+        });
+
+        it('should show name user', () => {
+            expect(screen.getByText('User Fake'));
+        });
+
+        it('should show avatar image', () => {
+            const containerImg = document.querySelector('#user-avatar');
+            expect(containerImg.getAttribute('src')).toEqual('https://avatar-fake.png');
+        });
+
+    });
+
+
 });
